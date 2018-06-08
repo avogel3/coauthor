0a35c7178384752d34c1c146f1083274c5909c8b
Andrew Vogel <andrewpvogel@gmail.com>
2018-06-08 15:30:49 -0400

    Update coauthor config file name

--- lib/coauthor/constants.rb
+++ lib/coauthor/constants.rb
@@ -1,6 +1,6 @@
 module Coauthor
   module Constants
-    PAIR_CONFIG_FILE = '~/.coauthors'.freeze
+    PAIR_CONFIG_FILE = '~/.coauthorrc'.freeze
     PAIR_FILE_PATH = File.expand_path(PAIR_CONFIG_FILE)
   end
 end
