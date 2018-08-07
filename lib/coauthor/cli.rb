e4d484fc0b349f4b17315b9f597c9b5f1bb8bca4
Andrew Vogel <andrewpvogel@gmail.com>
2018-08-07 15:00:27 -0400

    Add git_user class to set the git user for a repo

--- lib/coauthor/cli.rb
+++ lib/coauthor/cli.rb
@@ -12,11 +12,31 @@ module Coauthor
       Coauthor::Setup.new.call
     end
 
+    desc 'git_user', 'Set the default git user for the current repository'
+    def git_user
+      puts "Set this repository's git user".colorize(:blue)
+      name = prompt_user_name
+      email = prompt_user_email
+      Coauthor::GitUser.call(email: email, name: name)
+    end
+
     desc 'commit', 'Default command. Make a commit with the coauthor configuration'
     def commit
       return system("git commit --template #{PAIR_CONFIG_FILE}") if File.file?(PAIR_FILE_PATH)
       puts `echo "Run coauthor setup before committing with Coauthor"`
     end
     default_task :commit
+
+    private
+
+    def prompt_user_name
+      puts "Enter the user's name:".colorize(:yellow)
+      STDIN.gets.chomp
+    end
+
+    def prompt_user_email
+      puts "Enter the author's email:".colorize(:yellow)
+      STDIN.gets.chomp
+    end
   end
 end
