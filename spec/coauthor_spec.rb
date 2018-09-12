8edc4075e4a76905cc543d68a046efe1663078ed
Andrew <andrewpvogel@gmail.com>
2018-09-12 10:14:34 -0400

    Remove useless spec

--- spec/coauthor_spec.rb
+++ spec/coauthor_spec.rb
@@ -1,9 +1,5 @@
 RSpec.describe Coauthor do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Coauthor::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
   end
 end
